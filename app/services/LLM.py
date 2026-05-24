740f1512d1843ea235ecdb92d0352567d9ef76f5
tritai1 <tritai11111111@gmail.com>
2026-05-24 12:17:26 +0700

    up date llm and requirement

--- app/services/LLM.py
+++ app/services/LLM.py
@@ -1,4 +1,3 @@
-from langchain_ollama import ChatOllama
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
@@ -6,9 +5,8 @@ import os
 load_dotenv()
 
 def get_llm():
-    return ChatOllama(
-        model="mistral", 
+    return ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
         temperature=0.1,
-        base_url="http://localhost:11434" 
+        google_api_key=os.getenv("GOOGLE_API_KEY")
     )
-    
